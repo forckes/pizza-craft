c7028b6762f1fef865351614f515a59e9f593ade
forckes <forckes@proton.me>
2023-07-06 12:29:25 +0300

    create interface for data from backend

---
+++ src/types/data.interface.ts
@@ -0,0 +1,10 @@
+export interface IData {
+	id: string; //`cause i use mockapi backend, so it hasn't some methods for picking only one item from data
+	imageUrl: string;
+	name: string;
+	types: number[];
+	sizes: number[];
+	price: number;
+	category: number;
+	rating: number;
+}
