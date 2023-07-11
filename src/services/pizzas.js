d0cf31b8433b8e389b3f946fb114921daf0c8698
forckes <forckes@proton.me>
2023-07-11 22:23:39 +0300

    delete comment

--- src/services/pizzas.js
+++ src/services/pizzas.js
@@ -27,35 +27,3 @@ export const pizzasApi = createApi({
 });
 
 export const { useGetPizzasQuery, useGetPizzaQuery } = pizzasApi;
-
-// const fetchPizzas = async () => {
-// 	try {
-// 		const sortBy = sort.sortProperty.replace("-", "");
-// 		const order = sort.sortProperty.includes("-") ? "asc" : "desc";
-
-// 		const category = categoryId > 0 ? `category=${categoryId}` : "";
-
-// 		const search = searchValue !== "" ? `search=${searchValue}` : "";
-
-// 		let limit = 4;
-// 		if (window.innerWidth <= 1400) {
-// 			limit = 3;
-// 		}
-
-// 		const response = await fetch(
-// 			`${BASE_URL}?page=${currentPage}&limit=${limit}&${category}&sortBy=${sortBy}&order=${order}&${search}`
-// 		);
-// 		if (response.ok) {
-// 			console.log("FETCH"); //here
-// 			const data = await response.json();
-// 			setPizzas(data);
-// 			setProgress(progress + 100);
-// 		} else {
-// 			setProgress(0);
-// 			toast.error("Can't load pizzas");
-// 		}
-// 	} catch (error) {
-// 		setProgress(0);
-// 		toast.error("Something went wrong :(");
-// 	}
-// };
