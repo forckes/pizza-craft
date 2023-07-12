1a08cc01ec6c80c13fee246b3e7034193cf108b6
forckes <forckes@proton.me>
2023-07-12 18:43:02 +0300

    add persistGate to index

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+//App
 import App from "./components/App/App";
+//redux
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { persistor, store } from "./redux/store";
+//toastify
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
+//persist
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(
 	document.getElementById("root") as HTMLElement
@@ -12,7 +17,10 @@ const root = ReactDOM.createRoot(
 root.render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<App />
+			<PersistGate loading={null} persistor={persistor}>
+				<App />
+			</PersistGate>
+
 			<ToastContainer position='top-center' autoClose={2000} />
 		</Provider>
 	</React.StrictMode>
